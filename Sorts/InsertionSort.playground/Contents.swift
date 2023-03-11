ee967f649a3405f28109277a34506af029357e13
qq1823988089 <1823988089@qq.com>
2023-03-11 13:14:45 +0800

    update

---
+++ Sorts/InsertionSort.playground/Contents.swift
@@ -0,0 +1,4 @@
+let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+
+print(insertionSort(list, <))
+print(insertionSort(list, >))
---
+++ Sorts/InsertionSort.playground/Sources/InsertionSort.swift
@@ -0,0 +1,27 @@
+//
+//  InsertionSort.swift
+//  
+//
+//  Created by 乔一 on 2023/3/11.
+//
+
+import Foundation
+
+public func insertionSort<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+    guard array.count > 1 else { return array }
+    //函数参数为常量，函数体只能访问不能修改，这里复制一份
+    var sortedArray = array
+    //从第二个数开始
+    for index in 1..<sortedArray.count {
+        var currentIndex = index
+        //记录当前数
+        let temp = sortedArray[currentIndex]
+        //与前一个数比较，符合要求则赋值给当前数直到找到插入位置
+        while currentIndex > 0, isOrderedBefore(temp, sortedArray[currentIndex - 1]) {
+            sortedArray[currentIndex] = sortedArray[currentIndex - 1]
+            currentIndex -= 1
+        }
+        sortedArray[currentIndex] = temp
+    }
+    return sortedArray
+}
